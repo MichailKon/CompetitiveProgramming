6097f04a56af2035ef9d7453f09b452b6a661e20
MichailKon <misha2kon@mail.ru>
2021-05-05 08:59:12 +0300

    Create SparseTable.cpp
    Added sparse table

---
+++ DataStructures/SparseTable.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+template<class T, class C>
+class sparseTable {
+private:
+    unsigned int size;
+    std::vector <std::vector<T>> table;
+    std::vector<int> logs;
+    C *function;
+
+    void fillLogs() {
+        logs.assign(size + 1, 0);
+        logs.shrink_to_fit();
+        logs[1] = 0;
+        for (int i = 2; i <= size; i++) {
+            logs[i] = logs[i >> 1] + 1;
+        }
+    }
+
+    void fillTable() {
+        for (int k = 1; k <= logs[size]; k++) {
+            unsigned int nowSize = table[k - 1].size() - (1 << (k - 1));
+            table[k].resize(nowSize);
+            table[k].shrink_to_fit();
+            for (int i = 0; i < nowSize; i++) {
+                table[k][i] = function(table[k - 1][i], table[k - 1][i + (1 << (k - 1))]);
+            }
+        }
+    }
+
+public:
+    template<typename _RandomAccessIterator>
+    void build(const _RandomAccessIterator begin, const _RandomAccessIterator end) {
+        // build logs
+        size = end - begin;
+        fillLogs();
+        // build table
+        table.resize(logs[size] + 1);
+        table[0].resize(size);
+        table[0].shrink_to_fit();
+        for (int i = 0; i < size; i++) table[0][i] = *(begin + i);
+        fillTable();
+    }
+
+    sparseTable(const C &function) : function(function), size(0) {}
+
+    sparseTable() : function([](const T &a, const T &b) { return std::min(a, b); }), size(0) {}
+
+    unsigned int getSize() const {
+        return size;
+    }
+
+    T get(const int &l, const int &r) const {
+        int k = logs[r - l + 1];
+        return function(table[k][l], table[k][r - (1 << k) + 1]);
+    }
+};
+
+int f(const int &a, const int &b) {
+    return std::min(a, b);
+}
+
+int main() {
+//	std::vector<int> a = { 1, 2, 3, 2, 5 };
+    int a[] = {1, 2, 3, 2, 5};
+
+    sparseTable<int, decltype(f)> st(f);
+//	st.build(a.begin(), a.end());
+    st.build(a, a + 5);
+
+    std::cout << st.get(0, 1) << '\n';
+}
